a986ef940f98854c0d92acb75b43c69972a1a641
javier <hectorjavierpalacio@gmail.com>
2018-05-15 12:19:02 -0300

    archivo usuario

---
+++ usuario.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+	<link rel="stylesheet" href="css/estilos2.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+</head>
+<body>
+	<div class="contenedor">
+		<header>
+			<div class="logo">
+				<a href="paginaprincipal.php"><img src="img/logo.jpeg" width="150"></a>
+				<a href="paginaprincipal.php">Group Trip</a>
+				<a href="#" class="btn-menu"><i class="fas fa-bars"></i></a> 
+			</div>
+			<div class="enlaces">
+				<a href="#"><i class="fa fa-camera-retro" aria-hidden="true"></i>Fotos</a>
+				<a href="#"><i class="fa fa-plane" aria-hidden="true"></i>Destinos</a>
+				<a href="#"><i class="fa fa-users" aria-hidden="true"></i>Amigos</a>
+				<a href="#"><i class="fa fa-commenting-o" aria-hidden="true"></i>Sala de Chat</a>
+				<a href="#"><i class="fa fa-binoculars" aria-hidden="true"></i>Búsqueda de contactos</a>
+				
+			</div>
+			
+		</header>
+		
+		<main>
+			<article>
+				<h1>Bienvenido Usuario</h1>
+				<h2>Desarrollo página de usuario</h2>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pharetra nunc a lacus imperdiet facilisis sit amet vitae quam. Duis dapibus felis nisi, ut porttitor tortor semper in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis maximus faucibus. Praesent luctus ex justo, ut egestas diam rutrum ac. Nam posuere cursus congue. Pellentesque vulputate ipsum volutpat ex venenatis, a lacinia sem ultrices. Proin ac neque massa. Nullam molestie, tortor eu laoreet rhoncus, tellus elit tincidunt nulla, et tempor sem purus nec eros. In sagittis nibh ut tortor tempor, sed lacinia est eleifend. Etiam eu lectus lorem.<p>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pharetra nunc a lacus imperdiet facilisis sit amet vitae quam. Duis dapibus felis nisi, ut porttitor tortor semper in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis maximus faucibus. Praesent luctus ex justo, ut egestas diam rutrum ac. Nam posuere cursus congue. Pellentesque vulputate ipsum volutpat ex venenatis, a lacinia sem ultrices. Proin ac neque massa. Nullam molestie, tortor eu laoreet rhoncus, tellus elit tincidunt nulla, et tempor sem purus nec eros. In sagittis nibh ut tortor tempor, sed lacinia est eleifend. Etiam eu lectus lorem.<p>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pharetra nunc a lacus imperdiet facilisis sit amet vitae quam. Duis dapibus felis nisi, ut porttitor tortor semper in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis maximus faucibus. Praesent luctus ex justo, ut egestas diam rutrum ac. Nam posuere cursus congue. Pellentesque vulputate ipsum volutpat ex venenatis, a lacinia sem ultrices. Proin ac neque massa. Nullam molestie, tortor eu laoreet rhoncus, tellus elit tincidunt nulla, et tempor sem purus nec eros. In sagittis nibh ut tortor tempor, sed lacinia est eleifend. Etiam eu lectus lorem.<p>
+				<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pharetra nunc a lacus imperdiet facilisis sit amet vitae quam. Duis dapibus felis nisi, ut porttitor tortor semper in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis maximus faucibus. Praesent luctus ex justo, ut egestas diam rutrum ac. Nam posuere cursus congue. Pellentesque vulputate ipsum volutpat ex venenatis, a lacinia sem ultrices. Proin ac neque massa. Nullam molestie, tortor eu laoreet rhoncus, tellus elit tincidunt nulla, et tempor sem purus nec eros. In sagittis nibh ut tortor tempor, sed lacinia est eleifend. Etiam eu lectus lorem.<p>
+
+			</article>	
+		</main>
+		
+		<aside>
+			<div class="registrado">
+				<form class="" method="post" action="paginaprincipal.php">
+			        <h2 >Login Out</h2>
+			        
+                    <button type="submit" name="loguear" class="btn">Log Out</button>
+			    </form>
+      		</div>
+			
+		</aside>
+		<?php require_once 'footer.php'; ?>
+	</div>
+	
+	
+
+</body>
+</html>
